20b27aa1ddd31d7c1da528e82b17c598e8355a9a
KevOr <196833372+KevinOrlov@users.noreply.github.com>
2025-11-16 00:22:02 +0000

    finished the FAQs on the home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,34 @@
+// FadeToggle jQuery sections - collapsible section turn into FAQs into CSS accordion
+$(document).ready(function () {
+
+  // FAQ sections toggles
+  $("#lifeButton").click(function () {
+    $("#lifeFAQ").fadeToggle(750);
+  });
+
+  $("#carButton").click(function () {
+    $("#carFAQ").fadeToggle(750);
+  });
+
+  $("#homeButton").click(function () {
+    $("#homeFAQ").fadeToggle(750);
+  });
+
+});
+// END jQuery---------------------------
+
 // FAQs Accordion
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+    }
   });
 }
