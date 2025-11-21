589d4ec72134aee5db85ba1b31dba6f6e08f7ef1
Juan A. Boccia <jboccia@gmail.com>
2025-11-21 17:20:41 +0000

    Implement life form field validation.

--- life/assets/js/script.js
+++ life/assets/js/script.js
@@ -79,9 +79,63 @@ function triggerToast(toastType, toastText) {
 }
 
 // *** Handle form validation ***
-
-// 1 get submit button and prevent default behaviour
+// 1 get submit button
+document.getElementById("submit-form-btn").addEventListener("click", function(event){
 // 2 implement validation on the form
+event.preventDefault // Stop the form from sending information to its target
+let lifeForm = document.getElementById("lifeForm");
+let reqFields = lifeForm.querySelectorAll(".validate"); // identify inputs with a "validate" class
+for (field of reqFields) {
+    if (field.disabled == false){
+        console.log(field.type, "-", field.value, field.name)
+        let t = field.type;
+        switch(t){            
+            case "text":
+                if (field.value.trim() == "") {
+                    triggerToast("warning", `${field.name} is a mandatory field.`);
+                    return;
+                }
+                if (field.name == "Eircode") {
+                    field.value = field.value.trim().split(" ").join(""); // remove spaces at sides and also in the middle if found
+                    if (field.value.length < 7) {
+                        field.focus;
+                        triggerToast("warning", "Eircode length needs to be 7 characters long.")
+                        return;
+                    }
+                }
+                break;
+            case "email":
+                if (field.value.trim() == "") {
+                    triggerToast("warning", `${field.name} is a mandatory field.`);
+                    return;
+                }
+                if (field.value.search("@") == -1 || field.value.search(".") == -1){ //check for missing "@" and  missing "."
+                    triggerToast("warning", `${field.value} is not a valid email address.`);
+                    return
+                }
+                break;
+            case "date":
+                if (field.value == "") {
+                    triggerToast("warning", `The value given for Date of Birth is not valid`)
+                    return;
+                }
+                break;
+            case "radio":
+                // get both buttons and make sure that either one is selected
+                let buttons = field.parentNode.parentNode.querySelectorAll("input");
+                console.log(buttons)
+                // document.querySelectorAll(".firstSmoker");
+                if(!buttons[0].checked && !buttons[1].checked) {
+                    triggerToast("warning", `Please select "Yes" or "No" in the smoker status.`)
+                    console.log("both unchecked")
+                    return;
+                }
+
+                break;
+        }
+    }
+}
+})
 
 // TO DO
 // 1 implement emailjs
