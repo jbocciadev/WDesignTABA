fa34fa9e5ad9b169f0bfef7c46adec3a78444830
KevOr <196833372+KevinOrlov@users.noreply.github.com>
2025-11-23 23:06:22 +0000

    Amended javascript stucture, phone and reg validation UI friendly enchancements, FAQ link

--- car/assets/js/calculations.js
+++ car/assets/js/calculations.js
@@ -1,120 +1,3 @@
-// VALIDATIONS SECTION -------------------------------------------------------------------------
-//Reg validation -- WARN No spaces
-document.getElementById('inputReg').addEventListener('input', function () {
-    const reg = this.value;
-    const warning = document.getElementById('regWarning');
-
-    // Logical check for Reg input
-    const pattern = /^\d{2,3}-[A-Za-z]{1,2}-\d+$/; // 2 or 3 digits for year, one or two letters for County code, and dashes
-    // warning if conditions are and arent met
-    if (!pattern.test(reg)) {
-        warning.textContent = "Format must be like your Reg Plate: 181-D-123";
-        warning.style.display = "inline";
-    } else {
-        warning.style.display = "none";
-    }
-});
-
-// VALUE VALIDATION - PREVENT 200,000 plus car value 
-const carValueInput = document.getElementById("carValue");
-
-carValueInput.addEventListener("input", function () {
-    let value = parseFloat(this.value) || 0;
-    if (value > 200000) {
-        this.value = 200000;  // automatically clamp, limit
-    }
-});
-// END OF VALIDATIONS ------------------------------------------------------
-
-
-// DOM MANIPULATION SECTION-----------------------------------------------------------------------
-// REG PLATE - Source W3Schools - HTML DOM Element textContent
-const input = document.getElementById("inputReg");
-const mirror = document.getElementById("regCopy");
-
-input.addEventListener("input", () => {
-    regCopy.textContent = input.value; // dynamically update the span container
-});
-
-input.addEventListener("input", () => {
-    const cleaned = input.value.replace(/[\s]/g, '').toUpperCase(); //added .toUpperCase and removed symbols/spaces except dash, so reg looks good in the plate div
-    mirror.textContent = cleaned;
-});
-
-
-
-// FadeToggle jQuery sections - collapsible section turn into edit sections buttons when 'proceed' button clicked
-$(document).ready(function () {
-
-    // Proceed buttons -- Fade buttons in bacause i needed them hidden via CSS when page is initially loaded
-    $("#closeCarInfo").click(function () {
-        $("#section1").fadeOut(850); // hide the section
-        $("#editCarInfo").fadeIn(850); // show the edit buttons
-    });
-
-    $("#closeInsPref").click(function () {
-        $("#section2").fadeOut(850); // hide the section
-        $("#editInsPref").fadeIn(850); // show the edit buttons
-    });
-
-    $("#closeDriverInfo").click(function () {
-        $("#section3").fadeOut(850); // hide the section
-        $("#editDriverDetails").fadeIn(850); // show the edit buttons
-
-    });
-
-    // Edit buttons -- show section and hide the edit button
-    $("#editCarInfo").click(function () {
-        $("#section1").fadeIn(850);
-        $("#editCarInfo").fadeOut(850);
-
-    });
-
-    $("#editInsPref").click(function () {
-        $("#section2").fadeIn(850);
-        $("#editInsPref").fadeOut(850);
-
-    });
-
-    $("#editDriverDetails").click(function () {
-        $("#section3").fadeIn(850);
-        $("#editDriverDetails").fadeOut(850);
-    });
-});
-// END jQuery---------------------------
-
-// IMPORT DESTINATION SHOW SECTION
-importedSwitch.addEventListener('change', function () {
-    if (this.checked) {
-        importedCountry.style.display = 'block';
-    } else {
-        importedCountry.style.display = 'none';
-    }
-});
-
-// PENALTY POINT ADDITIONAL SECTION SHOW
-penaltySwitch.addEventListener('change', function () {
-    if (this.checked) {
-        penaltyArea.style.display = 'block';
-    } else {
-        penaltyArea.style.display = 'none';
-    }
-});
-
-// TERMS AND CONDITIONS OPEN OVERLAY/POP-UP
-$(document).ready(function () {
-
-    $("#openTC").click(function () {
-        $("#tcOverlay").fadeIn(750); // show the edit buttons
-    });
-
-    // TERMS AND CONDITIONS CLOSE OVERLAY -- Source Stackoverflow
-    $("#closeOverlay").click(function () {
-        $("#tcOverlay").fadeOut(750);
-    });
-});
-// END OF DOM MANIPULATIONS-------------------------------------------------------------
-
 // CALCULATIONS SECTION ---------------------------------------------------------------------------
 // Vehicle Information Section
 
@@ -123,23 +6,15 @@ function calculatePremium() {
     let total = 0;
     const carValue = parseFloat(document.getElementById("carValue").value || 0);
     // Determine the percentage based on car value ranges
-    let valueMultiplier = 0.075;
+    let valueMultiplier = 0;
     if (carValue <= 10000) {
-        valueMultiplier = 0.01;
-    } else if (carValue <= 20000) {
         valueMultiplier = 0.015;
-    } else if (carValue <= 30000) {
-        valueMultiplier = 0.017;
-    } else if (carValue <= 40000) {
-        valueMultiplier = 0.02;
-    } else if (carValue <= 60000) {
-        valueMultiplier = 0.025;
-    } else if (carValue <= 70000) {
-        valueMultiplier = 0.03;
-    } else if (carValue <= 80000) {
-        valueMultiplier = 0.04;
+    } else if (carValue <= 50000) {
+        valueMultiplier = 0.0175;
+    } else if (carValue <= 120000) {
+        valueMultiplier = 0.019;
     } else {
-        valueMultiplier = 0.07;
+        valueMultiplier = 0.125;
     }
 
     total = carValue * valueMultiplier; // Calculate the base premium
@@ -192,9 +67,9 @@ function calculatePremium() {
     const parking = parseInt(document.getElementById("parking").value || 0); // parse int the value of dropdown item
     let parkingMultiplier = 0; // default (no increase)
 
-    if (parking === 0) parkingMultiplier = -0.1;
+    if (parking === 0) parkingMultiplier = -0.2; // garaged
     else if (parking === 1) parkingMultiplier = 0;
-    else parkingMultiplier = 0.1; // garaged
+    else parkingMultiplier = 0.2; 
 
     total += total * parkingMultiplier; // Calculate the Parking premium
 
@@ -228,18 +103,17 @@ function calculatePremium() {
     total += total * driverAgeMultiplier
 
 
-
     // Driver Experience Calculations (DRIVER DETAILS SECTION)----------------------------------------------
 
     const experience = Math.max(0, parseInt(document.getElementById("experience").value || 0)); //parse values and make sure cant be minuses
     let experienceMultiplier = 0; // default (no increase)
 
-    if (experience === 0) experienceMultiplier = -0.04;
-    else if (experience === 1) experienceMultiplier = -0.08;
-    else if (experience === 2) experienceMultiplier = -0.12;
-    else if (experience === 3) experienceMultiplier = -0.16;
-    else if (experience === 4) experienceMultiplier = -0.2;
-    else experienceMultiplier = -0.4; // 5+
+    if (experience === 0) experienceMultiplier = -0.05;
+    else if (experience === 1) experienceMultiplier = -0.1;
+    else if (experience === 2) experienceMultiplier = -0.2;
+    else if (experience === 3) experienceMultiplier = -0.3;
+    else if (experience === 4) experienceMultiplier = -0.4;
+    else experienceMultiplier = -0.6; // 5+
 
     // Calculate the base premium
     total += total * experienceMultiplier;
@@ -289,14 +163,14 @@ function calculatePremium() {
     // User Required to pick one of them by HTML validation
     const rates = {
         comprehensive: 300,
-        fireTheft: 150,
+        fireTheft: 250,
         thirdOnly: 50
     };
     const policy = document.querySelector('input[name="policy"]:checked')?.value;
     if (policy) {
         total += rates[policy];
     }
-    let baseline = 300
+    let baseline = 150
     total += baseline;
 
 
@@ -307,7 +181,7 @@ function calculatePremium() {
     document.getElementById("premiumOutput").textContent = total.toFixed(2);
     document.getElementById("monthlyPremiumOutput").textContent = monthlyTotal.toFixed(2);
 
-    // RETURN the totals variables for the EmailJS
+    // RETURN the totals variables for the EmailJS below
     return { total, monthlyTotal };
 }
 
@@ -316,6 +190,7 @@ document.querySelectorAll("input, select").forEach(el => {
     el.addEventListener("input", calculatePremium);
     el.addEventListener("change", calculatePremium);
 });
+
 
 // // EMAILJS INTEGRATION -------------------------------------- Source: https://www.youtube.com/watch?v=ziYrGFADE1g&t=438s
 // emailjs.init("X0VrOze2ucHH5azPw");
